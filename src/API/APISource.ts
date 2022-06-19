60108ae0adda3a41a25230cd6c5508d881889fd4
franky3020 <210238hwsh@gmail.com>
2022-06-19 22:40:03 +0800

    Add TODO

--- src/API/APISource.ts
+++ src/API/APISource.ts
@@ -1,8 +1,9 @@
 
 
 
-
+// TODO: BASE_URL 改成可用環境變數指定
 const BASE_URL = "https://store.dogecoin.idv.tw/backend_server";
+// const BASE_URL = "http://localhost:5000"; // for test env
 
 export const API_URL = BASE_URL + "/api";
 export const STATIC_SOURCE_URL = BASE_URL + "/public";
