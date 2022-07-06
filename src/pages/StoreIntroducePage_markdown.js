533316dcc1c7a05d7c528c4eb29676e2eb02b191
franky3020 <210238hwsh@gmail.com>
2022-07-06 15:03:35 +0800

    fix url bug

--- src/pages/StoreIntroducePage_markdown.js
+++ src/pages/StoreIntroducePage_markdown.js
@@ -6,7 +6,7 @@ const markdown =
 
 目前因測試中, 所以每個商品都免費購買
 
-模型目前是拿 [thingiverse](www.thingiverse.com) 
+模型目前是拿 [thingiverse](https://www.thingiverse.com) 
 與 [fab365.net](https://fab365.net/) 上的來做填充
 
 未來將會放上我自己製作的模型
